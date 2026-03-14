6732b67497a2e9f6ca01a0e107787036143e1724
sathyakatrapalli2007 <sathyakatrapalli@gmail.com>
2026-03-14 10:56:30 +0530

    Add Settings class for game configuration

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,16 +3,23 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Creating a class that would manage all game attributes"""
     def __init__(self):
         """Creating a game object"""
         pygame.init()
+        self.settings=Settings()
 
         #Creating overall game window
-        self.screen=pygame.display.set_mode((1200,800))
+        self.screen=pygame.display.set_mode((self.settings.screen_width,
+                                             self.settings.screen_height))
         #Creating text to be displayed
         pygame.display.set_caption("Alien Invasion")
+        #Creating a class Clock object
+        self.clock=pygame.time.Clock()
+        #Setting background colour
 
     def run_game(self):
         """Creating a loop that will update the screen"""
@@ -20,8 +27,13 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            """Draws new screen"""
+            #Redraw the screen with the appropriate filling
+            self.screen.fill(self.settings.bg_color)
+            #Draws new screen
             pygame.display.flip()
+            #Keeps consistent framerate by ticking once through a loop
+            self.clock.tick(60)
+            
     
 if __name__=='__main__':
     ai=AlienInvasion()
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """ A class that stores all the game object settings"""
+    def __init__(self):
+        """Initialises different settings as attributes"""
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color=(231,196,213)
